bde73d420e86964597f9a1364e1a0fc91b9d385f
Sergey Popinevskiy <gurza000@gmail.com>
2020-06-24 12:56:26 +0300

    Init tests

---
+++ tests/test_client.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from alfastrah import AlfaStrahClient
+
+client = AlfaStrahClient()
+
+
+class TestBasic:
+    def test_client_create(self):
+        assert client is not None
