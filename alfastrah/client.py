8dc8cc14064bfc5d7f35d682574485d52958e112
Sergey Popinevskiy <gurza000@gmail.com>
2020-06-24 13:17:19 +0300

    Implement prototype of `request()` method

--- alfastrah/client.py
+++ alfastrah/client.py
@@ -5,5 +5,39 @@ import requests
 
 
 class AlfaStrahClient:
-    def __init__(self):
-        pass
+    API_HOST = ''
+
+    def __init__(self, api_key, verify_ssl=True):
+        self.api_key = api_key
+        self.verify_ssl = verify_ssl
+        self.req = self.resp = None
+
+    def request(self, method, endpoint, data=None):
+        """Constructs and sends a request to API Gateway.
+
+        :param method: HTTP method, e.g. 'GET', 'POST', 'PUT', 'DELETE'.
+        :type method: str
+        :param endpoint: API endpoint, e.g. '/travel-ext-services/api/v2/products'.
+        :type endpoint: str
+        :param data: (optional) dictionary of parameters to send in the query.
+        :param data: dict or None
+        :return: main content of the response API.
+        :rtype: object
+        """
+        headers = {
+            'api_key': self.api_key,
+        }
+        payload = json.dumps(data, cls=MultiJSONEncoder)
+        url = '{api_host}/{endpoint}'.format(api_host=self.API_HOST, endpoint=endpoint)
+        r = requests.request(method, url,
+                             headers=headers, data=payload, verify=self.verify_ssl)
+        self.resp = r.json()
+        return self.resp
+
+
+class MultiJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if hasattr(o, 'to_json') and callable(o.to_json):
+            return o.to_json()
+        # Let the base class default raise the TypeError
+        return json.JSONEncoder.default(self, o)
