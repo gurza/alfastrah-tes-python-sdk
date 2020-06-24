736f673fdd05043e4467856cf53a30b8f47061b4
Sergey Popinevskiy <gurza000@gmail.com>
2020-06-24 20:21:52 +0300

    Refactoring: rename

--- tests/test_client.py
+++ tests/test_client.py
@@ -12,14 +12,14 @@ client.api_host = 'https://uat-tes.alfastrah.ru'
 
 
 class TestBasic:
-    def test_client_create(self):
+    def test_create_client(self):
         assert client
 
     def test_api_access(self):
         products = client.get_products()
         assert products
 
-    def test_nonjson_response(self):
+    def test_handle_nonjson_response(self):
         with pytest.raises(TESException):
             client.request('GET', '/404')
         assert client.status_code == 404
