d6d1ad303b8ea25716754cba69145d056e627e18
Sergey Popinevskiy <gurza000@gmail.com>
2020-07-09 15:16:36 +0300

    Add `test_enum_list` to TestJsonSerialization

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,16 +7,20 @@ from .utils import load_response
 from tes import MultiJSONEncoder
 from tes import (
     ApiProblem, InsuranceProduct, Risk, RiskType,
+    QuoteRequest, SportKind,
 )
 
 
 class TestJsonSerialization:
-    def test_enum_to_json(self):
+    def test_enum_attribute(self):
         risk = Risk(type=RiskType.RISK_COVID)
-        risk_dict = {
-            'type': 'RISK_COVID',
-        }
-        assert json.dumps(risk, cls=MultiJSONEncoder) == json.dumps(risk_dict)
+        risk_dict = json.loads(json.dumps(risk, cls=MultiJSONEncoder))
+        assert risk_dict['type'] == 'RISK_COVID'
+
+    def test_enum_list(self):
+        quote = QuoteRequest(sport=[SportKind.COMMON_SPORT])
+        quote_dict = json.loads(json.dumps(quote, cls=MultiJSONEncoder))
+        assert quote_dict['sport'] == ['COMMON_SPORT']
 
 
 class TestAPIResponseHandling:
