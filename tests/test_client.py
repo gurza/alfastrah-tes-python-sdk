16e4427d71a4dcaee2c0bd2fbe536b64e1f0935e
Sergey Popinevskiy <gurza000@gmail.com>
2020-07-24 17:40:18 +0300

    Use fixtures

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,31 +1,24 @@
 # -*- coding: utf-8 -*-
-import os
-
 import pytest
 
-from tes import AlfaInsTESClient
 from tes import TESException
-
-api_key = os.getenv('ALFASTRAH_KEY')
-client = AlfaInsTESClient(api_key)
-client.api_host = 'https://uat-tes.alfastrah.ru'
 
 
 class TestBasic:
-    def test_create_client(self):
-        assert client
+    def test_create_client(self, client_connector):
+        assert client_connector
 
-    def test_api_access(self):
-        products = client.get_products()
+    def test_api_access(self, client_connector):
+        products = client_connector.get_products()
         assert products
 
-    def test_handle_nonjson_response(self):
+    def test_handle_nonjson_response(self, client_connector):
         with pytest.raises(TESException):
-            client.request('GET', '/404')
-        assert client.status_code == 404
+            client_connector.request('GET', '/404')
+        assert client_connector.status_code == 404
 
 
 class TestApi:
-    def test_get_products(self):
-        products = client.get_products(product_type='AIR')
+    def test_get_products(self, client_connector):
+        products = client_connector.get_products(product_type='AIR')
         assert products
