93f2296db6274cf7b412c8fefcd6bd3e5672b280
Sergey Popinevskiy <gurza000@gmail.com>
2020-07-31 09:39:41 +0300

    Up

--- tes/__version__.py
+++ tes/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'tes'
 __description__ = 'AlfaStrakhovanie TES API Python SDK'
 __url__ = ''
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'Sergey Popinevskiy'
 __author_email__ = 'sergey.popinevskiy@gmail.com'
 __license__ = 'MIT'
